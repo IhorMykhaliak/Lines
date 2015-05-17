e0a1d14d9649d2873f47703d6ca5e71aac6bd2f7
IhorMyhaliak <ihor123dn@gmail.com>
2015-05-17 13:14:11 +0300

    Update installer, put forms into the folder

--- Lines.DesktopUI/Forms/AboutGame.Designer.cs
+++ Lines.DesktopUI/Forms/AboutGame.Designer.cs
@@ -50,6 +50,7 @@
             this.rtxtHowToPlay.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.rtxtHowToPlay.Location = new System.Drawing.Point(12, 41);
             this.rtxtHowToPlay.Name = "rtxtHowToPlay";
+            this.rtxtHowToPlay.ReadOnly = true;
             this.rtxtHowToPlay.Size = new System.Drawing.Size(400, 330);
             this.rtxtHowToPlay.TabIndex = 1;
             this.rtxtHowToPlay.Text = resources.GetString("rtxtHowToPlay.Text");
